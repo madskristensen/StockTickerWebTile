f9c30912d442a8c0d9b84d35f9c9c8a1676dae98
Mads Kristensen <post@madskristensen.net>
2015-11-16 08:26:38 -0800

    Fixes to zip script

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-SET content=%1\icons %1\manifest.json
+SET content=.\%1\icons .\%1\manifest.json
 
-vendor\7z.exe a src\bin\%2.webtile %content% -r -mx9 -tZip
+vendor\7z.exe a .\%1\bin\%2.webtile %content% -r -mx9 -tZip
